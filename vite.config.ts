956118cad3e16b8b991778daea8130c977874999
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2025-11-27 00:55:01 +0200

    Initial commit
    - Create basic config for library deployment

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    dts({
+      tsconfigPath: resolve(__dirname, "tsconfig.lib.json"),
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['react'],
+    }
+  }
+})
