4b913307be2315865c84d0137c222f1638b630a1
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2025-11-29 15:03:37 +0200

    Setup testing env

---
+++ vitest.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    projects: [
+      // you can use a list of glob patterns to define your projects
+      // Vitest expects a list of config files
+      // or directories where there is a config file
+      'packages/*',
+      'tests/*/vitest.config.{e2e,unit}.ts',
+      // you can even run the same tests,
+      // but with different configs in the same "vitest" process
+      {
+        test: {
+          name: 'happy-dom',
+          root: './lib/tests',
+          environment: 'happy-dom',
+        },
+      },
+      {
+        test: {
+          name: 'node',
+          root: './lib/store/tests',
+          environment: 'node',
+        },
+      },
+    ],
+  },
+})
