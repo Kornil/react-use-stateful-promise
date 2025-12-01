6c4c2610aaf992680bf6f5ad4625553412ecc470
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2025-12-01 13:41:51 +0200

    Rename /dist .js file to index

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
+      formats: ['es'],
+      fileName: 'index',
     },
     copyPublicDir: false,
     rollupOptions: {
